67b7c7b51585964e846bd67659bbd2d1856de565
Anasoud1 <anasoudsassi@gmail.com>
2024-02-04 18:08:21 +0100

    updating

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,11 @@ Flask App
 """
 from flask import Flask, make_response, jsonify
 from models import storage
-from api.v1.views import app_views, app_views_states
+from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
-app.register_blueprint(app_views_states, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
