12a426b44f213fb049f7b840beeaaa4ec9daf03b
Anasoud1 <anasoudsassi@gmail.com>
2024-02-04 18:58:35 +0100

    adding cities import in __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,4 @@ app_views_cities = Blueprint('app_views_cities', __name__,
                              url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
